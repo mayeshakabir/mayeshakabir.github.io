a91f845e4a9cc2981d4d54dd80dbb69bf5d34270
Mayesha Kabir <mayeshakabir@Mayeshas-MacBook-Pro.local>
2018-12-13 00:45:09 -0800

    added bootstrap

--- js/main.js
+++ js/main.js
@@ -1,134 +1,16 @@
 $(document).ready(function(){
 
-  //BOUNCE ICONS:
-  $('.icon').hover(function(){
-     $(this).toggleClass('animated bounce');
-  });
-
-  $('.title-logo').hover(function(){
-     $(this).toggleClass('animated bounce');
-  });
-
-  $('.nav').click(function() {
+  $('.nav-link').click(function() {
       var id = $(this).attr('id');
       $('html, body').animate({
           scrollTop: ($('.' + id).offset().top)
       });
   });
 
-  /*
-  //use for loop
-  $('#game1-pic').css('opacity', 0);
- 
-  $('#game1-pic').waypoint(function() {
-      $('#game1-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
+  $('.btn').click(function() {
+      $('html, body').animate({
+          scrollTop: ($('#portfolio').offset().top)
+      });
+  });
 
-  $('#game1-text').css('opacity', 0);
- 
-  $('#game1-text').waypoint(function() {
-      $('#game1-text').addClass('fadeInRight');
-  }, { offset: '90%' });
-
-
-
-  //ICON FROM LEFT:
-  $('#game1-pic').css('opacity', 0);
- 
-  $('#game1-pic').waypoint(function() {
-      $('#game1-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
-
-  $('#game2-pic').css('opacity', 0);
- 
-  $('#game2-pic').waypoint(function() {
-      $('#game2-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
-
-  $('#app1-pic').css('opacity', 0);
- 
-  $('#app1-pic').waypoint(function() {
-      $('#app1-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
- 
-  $('#app2-pic').css('opacity', 0);
- 
-  $('#app2-pic').waypoint(function() {
-      $('#app2-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
-
-  $('#app3-pic').css('opacity', 0);
- 
-  $('#app3-pic').waypoint(function() {
-      $('#app3-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
- 
-  $('#des1-pic').css('opacity', 0);
- 
-  $('#des1-pic').waypoint(function() {
-      $('#des1-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
- 
-  $('#des2-pic').css('opacity', 0);
- 
-  $('#des2-pic').waypoint(function() {
-      $('#des2-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
- 
-  $('#des3-pic').css('opacity', 0);
- 
-  $('#des3-pic').waypoint(function() {
-      $('#des3-pic').addClass('fadeInLeft');
-  }, { offset: '90%' });
-
-  //TEXT FROM RIGHT:
-  $('#game1-text').css('opacity', 0);
- 
-  $('#game1-text').waypoint(function() {
-      $('#game1-text').addClass('fadeInRight');
-  }, { offset: '90%' });
-
-  $('#game2-text').css('opacity', 0);
- 
-  $('#game2-text').waypoint(function() {
-      $('#game2-text').addClass('fadeInRight');
-  }, { offset: '90%' });
-
-  $('#app1-text').css('opacity', 0);
- 
-  $('#app1-text').waypoint(function() {
-      $('#app1-text').addClass('fadeInRight');
-  }, { offset: '90%' });
- 
-  $('#app2-text').css('opacity', 0);
- 
-  $('#app2-text').waypoint(function() {
-      $('#app2-text').addClass('fadeInRight');
-  }, { offset: '90%' });
-
-  $('#app3-text').css('opacity', 0);
- 
-  $('#app3-text').waypoint(function() {
-      $('#app3-text').addClass('fadeInRight');
-  }, { offset: '90%' });
- 
-  $('#des1-text').css('opacity', 0);
- 
-  $('#des1-text').waypoint(function() {
-      $('#des1-text').addClass('fadeInRight');
-  }, { offset: '90%' });
- 
-  $('#des2-text').css('opacity', 0);
- 
-  $('#des2-text').waypoint(function() {
-      $('#des2-text').addClass('fadeInRight');
-  }, { offset: '90%' });
- 
-  $('#des3-text').css('opacity', 0);
- 
-  $('#des3-text').waypoint(function() {
-      $('#des3-text').addClass('fadeInRight');
-  }, { offset: '90%' });
-  */
- 
 });
