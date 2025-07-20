e46b99be36b59ac0d9aa007757193629e589fe5b
Mayesha Kabir <mayeshakabir@gmail.com>
2025-07-19 19:53:47 -0700

    update to react

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>
+          👩🏻‍💻 Mayesha Kabir
+        </h1>
+        <p className="about">
+          Hey I'm Mayesha! I'm a Software Engineer currently working @ <a href="https://www.lattice.com" target="_blank" rel="noopener noreferrer">Lattice</a>. I graduated from the University of British Columbia, majoring in Computer Science and am always looking new opportunities in software engineering.
+          <br />
+          <br />
+          My experiences include working as an Software Developer on the Alexa Guard team @ <a href="https://www.amazon.com/b?ie=UTF8&node=18021383011" target="_blank" rel="noopener noreferrer">Amazon</a> and I also previously interned @ <a href="https://hootsuite.com" target="_blank" rel="noopener noreferrer">Hootsuite</a>, <a href="https://blackbirdinteractive.com" target="_blank" rel="noopener noreferrer">Blackbird Interactive</a> & <a href="https://www.eastsidegames.com" target="_blank" rel="noopener noreferrer">East Side Games</a>! Links + games I created below!
+        </p>
+        <p className="links">  
+          <a href="/resume/mayesha-kabir-resume.pdf" target="_blank" rel="noopener noreferrer">resume</a>
+          <a href="http://www.github.com/mayeshakabir" target="_blank" rel="noopener noreferrer">github</a>
+          <a href="https://www.linkedin.com/in/mayeshakabir/" target="_blank" rel="noopener noreferrer">linkedIn</a>
+          <a href="https://medium.com/@mayesha.kabir" target="_blank" rel="noopener noreferrer">medium</a>
+          <a href="https://mayesha.itch.io" target="_blank" rel="noopener noreferrer">itch.io</a>
+          <a href="https://devpost.com/mayeshak" target="_blank" rel="noopener noreferrer">devpost</a>
+          <a href="mailto:mayeshakabir@gmail.com" target="_blank" rel="noopener noreferrer">email</a>
+        </p>
+
+        <div style={{ textAlign: 'center' }}>
+          <iframe 
+            title="Jewel Thief"
+            frameBorder="0" 
+            src="https://itch.io/embed/198640?bg_color=f2f2f2&amp;link_color=8080ff&amp;border_color=f2f2f2" 
+            width="208" 
+            height="167"
+          >
+            <a href="https://mayesha.itch.io/jewel-thief">Jewel Thief by mayesha's games</a>
+          </iframe>
+          <iframe 
+            title="Pre-Veal"
+            frameBorder="0" 
+            src="https://itch.io/embed/570447?bg_color=f2f2f2&amp;link_color=8080ff&amp;border_color=f2f2f2" 
+            width="208" 
+            height="167"
+          >
+            <a href="https://mayesha.itch.io/pre-veal">Pre-Veal by mayesha's games</a>
+          </iframe>
+          <iframe 
+            title="I Die You Die"
+            frameBorder="0" 
+            src="https://itch.io/embed/200080?bg_color=f2f2f2&amp;link_color=8080ff&amp;border_color=f2f2f2" 
+            width="208" 
+            height="167"
+          >
+            <a href="https://mayesha.itch.io/i-die-you-die">I Die You Die by mayesha's games</a>
+          </iframe>
+        </div>
+
+        <p className="copyright">
+          Mayesha Kabir © 2025
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
